7be015068aeb87937013f78c524660f563336011
Andrey-oss <62800423+Andrey-oss@users.noreply.github.com>
2025-06-22 12:01:04 +0300

    Refactored some part of code (by the way WIP)

--- matrix.py
+++ matrix.py
@@ -1,40 +1,48 @@
-def check_matrix(matrix: list): # Check if matrix is square
-    # It could be a decorator, but not now :)
+'''Module for matrix handling'''
+
+def check_matrix(matrix: list):
+    '''Check if matrix is square'''
+
     for i in matrix:
         if len(i) != len(matrix):
             raise ValueError("Matrix isn't square")
 
-def transpose_matrix(matrix: list) -> list: # From vertical to horizontal or vica versa
+def transpose_matrix(matrix: list) -> list:
+    '''Transpose matrix from vertical to horizontal or vica versa'''
+
     check_matrix(matrix)
     return [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix))]
 
-def add_zeros(matrix: list) -> list: # Add zeros to matrix after gauss algorithm
-    for i in range(len(matrix)):
-        for j in range(len(matrix)-len(matrix[i])):
-            matrix[i].insert(0, 0)
+def add_zeros(matrix: list) -> list:
+    '''Add zeros to matrix after gauss algorithm'''
+
+    for i, mi in enumerate(matrix):
+        for j in range(len(matrix)-len(mi)):
+            mi.insert(0, 0)
     return matrix
 
 def swap(matrix: list, idx: int) -> list:
+    '''Some swapping'''
+
     if idx >= len(matrix):
         raise IndexError(f"Index {idx} must be lower than length of matrix {len(matrix)}")
-    
-    if type(idx) != int:
+
+    if isinstance(idx, int) is not True:
         raise TypeError("Index must be integer only, not", type(idx))
-    
+
     if idx < 0:
         raise IndexError(f"Index {idx} must be >= 0")
 
     matrix = transpose_matrix(matrix)
 
     for i in range(idx+1):
-        '''temp_row = matrix[0]
-        matrix[0] = matrix[i]
-        matrix[i] = temp_row'''
         matrix[0], matrix[i] = matrix[i], matrix[0]
-    
+
     return transpose_matrix(matrix)
 
 def minor(matrix: list, idx: int) -> dict:
+    '''Get matrix minor by index'''
+
     m = {}
     matrix = swap(matrix, idx)
     oc = len(matrix)-1 # Output counter
@@ -42,10 +50,20 @@ def minor(matrix: list, idx: int) -> dict:
     return m
 
 def full_return(matrix: list, letter='x') -> dict:
+    '''Beautiful and full return of result'''
+
     mat = {f'{letter}{i+1}':matrix[i] for i in range(len(matrix))}
     return mat
 
-def gauss_method_algo(eq_system: list, eq_matrix: list, res1=[], res2=[]) -> list:
+def gauss_method_algo(eq_system: list, eq_matrix: list, res1=None, res2=None) -> list:
+    '''Function which describes gauss method'''
+
+    if res1 is None:
+        res1 = []
+
+    if res2 is None:
+        res2 = []
+
     check_matrix(eq_system)
     matrix1=[]
     matrix2=[]
@@ -56,27 +74,26 @@ def gauss_method_algo(eq_system: list, eq_matrix: list, res1=[], res2=[]) -> lis
     k = 1 # Common divisor to do zeros
 
     # Firstly we have to do common divisor
-    for i in range(len(eq_system)):
-        k *= eq_system[i][0]
+    for i, eq in enumerate(eq_system):
+        k *= eq[0]
 
     # Then we must get multiplication factor
-    for i in range(len(eq_system)):
-        if eq_system[i][0] == 0:
+    for i, eq in enumerate(eq_system):
+        if eq[0] == 0:
             raise ValueError("Zero pivot encountered! Try row swapping or check input data. Maybe system doesn't have solutions")
-        if type(eq_system[i][0]) != int and type(eq_system[i][0]) != float:
+        if isinstance(eq[0], int) and isinstance(eq[0], float):
             raise TypeError("Matrix contains non-integer symbols")
-        factor = k / eq_system[i][0]
+        factor = k / eq[0]
 
-        matrix1.append([eq_system[i][j]*factor for j in range(len(eq_system))])
+        matrix1.append([eq[j]*factor for j in range(len(eq_system))])
         matrix2.append(eq_matrix[i]*factor)
-    
+
     # Do some arithmetic operations
     for i in range(len(eq_system)-1):
         matrix1[i+1] = [matrix1[0][j]-matrix1[i+1][j] for j in range(len(eq_system))]
         matrix2[i+1] = matrix2[0]-matrix2[i+1]
 
     # Write data to result
-    #result[matrix2[0]] = matrix1[0] # here is a bug, cuz this are two zeros, only one can be written, and this one is the last. See to hash-table/dict documentation
     res1.append(matrix2[0])
     res2.append(matrix1[0])
     matrix1 = minor(matrix1, 0)[matrix1[0][0]]
@@ -84,18 +101,14 @@ def gauss_method_algo(eq_system: list, eq_matrix: list, res1=[], res2=[]) -> lis
     return gauss_method_algo(eq_system=matrix1, eq_matrix=matrix2, res1=res1, res2=res2)
 
 def gauss_method(eq_system: list, eq_matrix: list) -> list:
+    '''Absolute gauss method, which will run until the end'''
+
     result = gauss_method_algo(eq_system, eq_matrix)
     matrix = transpose_matrix(transpose_matrix(add_zeros([i for i in result[0]]))[::-1]) # What a hell
     solves = [k for k in result[1]][::-1]
     matrix = matrix[::-1]
     result = gauss_method_algo(matrix, solves, res1=[], res2=[])
 
-    '''output = []
-    for i in range(len(result[0])):
-        output.append(result[1][i]/result[0][i][0])
-
-    output = output[::-1] '''
-
     output = [result[1][i]/result[0][i][0] for i in range(len(result[0]))][::-1]
-    
+
     return output
